4dd3a803b06ed5bda91fe34115c8fbd763fe6cf3
xiaozefeng <qq523107430@163.com>
2018-05-25 18:18:40 +0800

    init

--- log_process.go
+++ log_process.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"os"
+	"bufio"
+	"io"
 )
 
 type Reader interface {
-	Read(rc chan string)
+	Read(rc chan []byte)
 }
 
 type Writer interface {
@@ -23,45 +26,69 @@ type WriteToInfluxDB struct {
 
 // 使用引用的话可以不用发生值拷贝
 // 并且如果需要修改这个结构体本身的一些属性时，可以直接使用l变量去修改
-func (r *ReadFromFile) Read(rc chan string) {
-	// 读取模块
-	rc <- "message"
+func (r *ReadFromFile) Read(rc chan []byte) {
+	// 打开文件
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file err:%s", err.Error()))
+	}
+	// 移动到文件末尾
+	f.Seek(0, 2)
+
+	// 从文件末尾开始逐行读取文件内容
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Microsecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("open file err:%s", err.Error()))
+		}
+		// 读取模块
+		rc <- line[:len(line)-1]
+	}
 }
 func (w WriteToInfluxDB) Write(wc chan string) {
 	// 写入模块
-	fmt.Println(<-wc)
+	for v := range wc{
+		fmt.Println(v)
+	}
 }
 
 func main() {
 	writer := &WriteToInfluxDB{
 		dataSource: "username&password",
 	}
-	reader := &ReadFromFile{path: "path"}
-
+	reader := &ReadFromFile{
+		path: "./access.log",
+	}
 
 	lp := &LogProcess{
-		rc:   make(chan string),
-		wc:   make(chan string),
-		writer:writer,
-		reader:reader,
+		rc:     make(chan []byte),
+		wc:     make(chan string),
+		writer: writer,
+		reader: reader,
 	}
 
 	go lp.reader.Read(lp.rc)
 	go lp.Process()
 	go lp.writer.Write(lp.wc)
 
-	time.Sleep(time.Second * 1)
+	time.Sleep(time.Second * 30)
 }
 
 type LogProcess struct {
-	rc     chan string // read chan
+	rc     chan []byte// read chan
 	wc     chan string // write chan
 	writer Writer
 	reader Reader
 }
 
 func (l *LogProcess) Process() {
-	// 解析模块j
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	// 解析模块
+	for v := range l.rc {
+		l.wc <- strings.ToUpper(string(v))
+	}
 }
